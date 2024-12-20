237980c35f5abe93a398daf9b3a4d4f23e2bb629
Nickjasonsp <nickjasonsp@gmail.com>
2024-12-19 22:09:10 -0300

    configurando sql server

---
+++ SalesWebMvc/SalesWebMvc/Models/SalesWebMvcContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using SalesWebMvc.Models;
+using System.IO;
+
+namespace SalesWebMvc
+{
+    public class SalesWebMvcContextFactory : IDesignTimeDbContextFactory<SalesWebMvcContext>
+    {
+        public SalesWebMvcContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<SalesWebMvcContext>();
+            var connectionString = configuration.GetConnectionString("SalesWebMvcContext");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new SalesWebMvcContext(optionsBuilder.Options);
+        }
+    }
+}

770a0bb1747eea1f24c653b578e02fb5275e777f
Nickjasonsp <nickjasonsp@gmail.com>
2025-01-09 21:23:20 -0300

    Integridade referencial

---
+++ SalesWebMvc/SalesWebMvc/Migrations/20250110000711_DepartmentForeignKey.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepartmentForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentID",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentID",
+                table: "Seller",
+                newName: "DepartmentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_DepartmentID",
+                table: "Seller",
+                newName: "IX_Seller_DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentId",
+                table: "Seller",
+                newName: "DepartmentID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_DepartmentId",
+                table: "Seller",
+                newName: "IX_Seller_DepartmentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentID",
+                table: "Seller",
+                column: "DepartmentID",
+                principalTable: "Department",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
